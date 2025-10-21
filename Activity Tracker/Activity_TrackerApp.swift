4feb64dd59ad4e5045bb07713c9c07d6cb6c7ff4
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-10-21 12:32:33 +0200

    Initial Commit

---
+++ Activity Tracker/Activity_TrackerApp.swift
@@ -0,0 +1,16 @@
+// 
+//  Activity_TrackerApp.swift
+//  Activity Tracker
+//
+    
+
+import SwiftUI
+
+@main
+struct Activity_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
